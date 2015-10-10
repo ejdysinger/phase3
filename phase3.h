3dd2b06b5364b106d2d40ce40326e6e4b6a0fb65
ejdysinger@gmail.com <ejdysinger@gmail.com>
2015-10-10 16:18:35 -0700

    added semaphore struct to phase3.h

--- phase3.h
+++ phase3.h
@@ -7,5 +7,24 @@
 
 #define MAXSEMS         200
 
+typedef struct semaphore{
+	int maxValue;
+	int current;
+	mailLine * waitList;
+} semaphore;
+
+typedef struct mailLine
+{
+    int PID;
+    // a pointer to the location in memory where the sent message is stored
+    char msg[MAX_MESSAGE];
+    // the max message size that can be held
+    int msgSize;
+    // the status of the mailLine object
+    int status;
+    // a pointer to the next mailLine object
+    struct mailLine * next;
+}mailLine;
+
 #endif /* _PHASE3_H */
 
